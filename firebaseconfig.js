ce0f6acf17cffe54377dc277c119c9701f79c987
sodeeq olapade <olapadeabiodun21@email.com>
2019-07-05 15:07:47 +0100

    Adds firebase
    Installs firebase SDK
    Adds firebaseconfig file to root

---
+++ firebaseconfig.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB9npsDCbvjvk04W8oH9WfYYfRZknFN1j8",
+    authDomain: "blake-e550d.firebaseapp.com",
+    databaseURL: "https://blake-e550d.firebaseio.com",
+    projectId: "blake-e550d",
+    storageBucket: "",
+    messagingSenderId: "499728572938",
+    appId: "1:499728572938:web:f04f57307e1f5d5a"
+  };
+
+
+
+  export default firebaseConfig;
