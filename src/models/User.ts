309561aefede48d34593f2dafea45709e010ff6d
sodeeq olapade <olapadeabiodun21@email.com>
2019-08-01 05:19:04 +0100

    add phone and address fields to user schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,37 +2,45 @@ import mongoose from 'mongoose';
 import { Usermodel } from '../typings/user';
 
 const UserSchema = new mongoose.Schema({
-    /** 
+  /** 
      *   Everything in Mongoose starts with a Schema. Each schema maps 
   to a MongoDB collection and defines the shape of the documents
   within that collection.
     */
 
-   name: {
+  name: {
     type: String,
     required: true,
     min: 3,
-    max: 125
+    max: 125,
   },
 
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
 
   password: {
     type: String,
     min: 8,
     max: 16,
-    required: true
+    required: true,
+  },
+
+  phone: {
+    type: String,
+    required: true,
+    length: 11,
+  },
+
+  address: {
+    type: String,
   },
 
   avatar: {
-    type: String
-  }
-
+    type: String,
+  },
 });
-
 
 export default mongoose.model<Usermodel>('User', UserSchema);
